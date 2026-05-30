f81971ae6e5b834395c3517419ca7d090f3af218
sergio <sopekof@gmail.com>
2026-05-30 12:10:43 -0300

    curso de python midudev

---
+++ 01_basic/01_print.py
@@ -0,0 +1,20 @@
+# El modulo print() se utiliza para mostrar información en la consola.
+# Es una función incorporada en Python que permite imprimir texto,
+# números u otros tipos de datos.
+print("¡Hola Mundo!")
+print("Esto tambein funciona con comillas simples.")
+
+print(" ")
+
+print("Python", "Español", "Genial")
+
+# Si queremos cambiar el separador entre los elementos,
+# podemos usar el argumento 'sep' en la función print().
+# Por defecto, el separador es un espacio,
+# pero podemos cambiarlo a cualquier otro carácter o cadena de caracteres.
+print("Separador", "con", "guion", sep="-")
+
+# El argumento 'end' se utiliza para especificar lo que se imprime al
+# final de la salida.
+print("Esto se imprime", end=" ")
+print("en la misma línea.")
