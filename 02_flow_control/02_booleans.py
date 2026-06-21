0af4f91250e1cf82ee2f5d86297c4db3f1c87753
SergioDeb <sopekof@gmail.com>
2026-06-21 10:51:14 -0300

    op logicos

---
+++ 02_flow_control/02_booleans.py
@@ -0,0 +1,34 @@
+###
+# Booleanos en Python
+# 02 Boolean
+# Valores lógicos: True y False
+# Fundamentales para el control de flujo y lógica en programación
+###
+
+print("\n ------- Booleanos en Python ------- \n")
+print("Valores booleanos básicos.")
+print("True:", True)
+print("False:", False)
+
+# Operadores de comparación
+
+print("5 > 4:", 5 > 4)  # True
+print("5 < 4:", 5 < 4)  # False
+print("5 == 5:", 5 == 5)  # True
+print("5 != 4:", 5 != 4)  # True
+print("5 >= 5:", 5 >= 5)  # True
+print("5 <= 4:", 5 <= 4)  # False
+
+# Comparación de cadenas
+print("\nConparación de cadenas")
+print("Manzana < Pera", "Manzana" < "Pera")  # True (orden alfabético)
+print("'Hola' == 'hola'", "Hola" == "hola")  # False (diferencia de mayúsculas)
+
+# Operadores lógicos
+print("\nOperadores lógicos\n")
+print("True and False:", True and False)  # False
+print("True or False:", True or False)  # True
+print("not True:", not True)  # False
+print("not False:", not False)  # True
+print("True and True:", True and True)  # True
+print("False or False:", False or False)  # False
