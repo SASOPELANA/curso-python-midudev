173e7e919be36cd16c690a9c9439e768acccddb7
sergio <sopekof@gmail.com>
2026-07-17 15:19:10 -0300

    add - element - insert

---
+++ 02_flow_control/04_list_methods.py
@@ -0,0 +1,24 @@
+###
+# Métodos
+###
+
+# 04 - Listas métodos
+# Los métodos mas importantes para trabajar con listas son:
+
+# Añadir o insertar elementos a la lista
+print("Metodos de listas\n")
+lista1 = [1, 2, 3, 4, 5]
+
+# Añadir elementos al final de lista
+lista1.append(6)
+print(lista1)
+
+# Insertar un elemento en la posición que le indiquemos como
+# primer argumento
+
+lista1.insert(1, 22)
+print(lista1)
+
+# Agregar elementos al final de la lista
+lista1.extend([7, 8])
+print(lista1)
