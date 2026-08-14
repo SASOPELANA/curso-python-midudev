97b4288beeb46896498738594f312e0bf5bf5b3e
Sergio <sopekof@gmail.com>
2026-08-14 10:26:01 -0300

    list comprehension

--- 03_loops/02_loop_for.py
+++ 03_loops/02_loop_for.py
@@ -47,3 +47,14 @@ for idx, animal in enumerate(animales):
     if animal == "loro":
         continue
     print(animal)
+
+# Comprensión de listas (list comprehension)
+print("\nList comprehension:")
+animales = ["perro", "gato", "raton", "loro", "pez", "canario"]
+animales_mayusculas = [animal.upper() for animal in animales]
+print("\nAnimales en mayúsculas:", animales_mayusculas)
+
+# Muestra los números pares de una lista del 0 al 10 usando list comprehension
+print("\nNúmeros pares de una lista:")
+pares = [num for num in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] if num % 2 == 0]
+print(pares)
