500300ac3d8d1bfa17502c6e454a7860024131f9
Sergio <sopekof@gmail.com>
2026-08-16 18:28:29 -0300

    logica

---
+++ 04_logic/02_challenge_jurassic_park.py
@@ -0,0 +1,38 @@
+"""
+En Jurassic Park, se ha observado que los dinosaurios carnívoros, como el temible T-Rex,
+depositan un número par de huevos.
+Imagina que tienes una lista de números enteros en la que cada número representa la
+cantidad de huevos puestos por un dinosaurio en el parque.
+
+Importante: Solo se consideran los huevos de los dinosaurios carnívoros (T-Rex)
+aquellos números que son pares.
+
+Objetivo:
+Escribe una función en Python que reciba una lista de números enteros y devuelva la suma
+total de los huevos que pertenecen a los dinosaurios carnívoros
+(es decir, la suma de todos los números pares en la lista).
+"""
+
+print("\nJurassic Park - Reto de lógica 2: \n")
+# Para ver si un número es par
+# siempre usamos el módulo %
+# nos da el resto de la división: eggs % 2 == 2
+
+
+def contar_huevos_dinosaurios_carnivoros(huevos_list) -> int:
+    """
+    Esta función recibe una lista de numeros enteros que representan la cantidad de huevos que han puesto diferentes dinosaurios en el parque jurásico y los de número par son de carnívoros. Devuelve un número con la suma de todos los huevos de carnívoros.
+    """
+
+    total_huevos_carnivoros = 0
+
+    for huevos in huevos_list:
+        if huevos % 2 == 0:
+            total_huevos_carnivoros += huevos
+
+    return total_huevos_carnivoros
+
+
+huevos_list = [2, 3, 4, 5, 15, 23, 1, 3]
+
+print(contar_huevos_dinosaurios_carnivoros(huevos_list))  # --> 6
